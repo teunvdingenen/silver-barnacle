6dd2f7c6a26663376c60bb5b6d41799ec3b56577
Teun van Dingenen <teunvdingenen@gmail.com>
2017-05-02 13:25:52 +0000

    added test scrpt

--- project_tst.py
+++ project_tst.py
@@ -38,36 +38,8 @@ def wave(strip, frame):
 if __name__ == '__main__':
 	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 	strip.begin()
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(INPUT_PIN, GPIO.IN)
-
 	while True:
-		FRAME+= 0.0008
-		if GPIO.input(INPUT_PIN):
-			if RINGTIMER == 0:
-				requests.get("http://localhost:4567/?ring")
-			RINGTIMER += 1
-			if RINGTIMER > 8000:
-				RINGTIMER = 0
-		if RING:
-			AMP += 0.00005		
-			TIMER += 1
-		else:
-			AMP -= 0.0001
-		if AMP > 0.5:
-			AMP = 0.5
-		elif AMP < 0:
-			AMP = 0
-
-		if FRAME % 1 == 0:
-			FRAME = 0
-
-		if TIMER > 5000:
-			TIMER = 0
-			RING = False
-
-		wave(strip, FRAME)
-		if os.path.exists('ring'):
-			RING = True
-			TIMER = 0
-			os.remove('ring')
+		for j in range(0, strip.numPixels()):
+			color = Color(RED,GREEN,BLUE)
+			strip.setPixelColor(j,color)
+		strip.show()
