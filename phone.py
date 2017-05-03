43f8bca6bc497751b106824b2da326e079a276ea
Teun van Dingenen <teunvdingenen@gmail.com>
2017-05-03 12:30:19 +0000

    simplyfied version

--- phone.py
+++ phone.py
@@ -6,51 +6,14 @@ import subprocess
 
 import time
 import RPi.GPIO as GPIO
-from math import *
-from neopixel import *
-
-# LED strip configuration:
-LED_COUNT      = 24      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 INPUT_PIN = 27
-
-RED 		= 255
-GREEN		= 165
-BLUE		= 0
-
-FRAME = 0
-AMP = 0
+OUTPUT_PIN = 23
 
 RING = False
 TIMER = 0
 
 RINGTIMER = 0
-HORNTIMER = 0
-
-HORN_LIFTED = False
-
-def wave(strip, frame):
-	for j in range(strip.numPixels()):
-		c = AMP * sin(2*pi*frame + j) + AMP
-		color = Color(int(RED*c),int(GREEN*c),int(BLUE*c))
-		strip.setPixelColor(j,color)
-	strip.show() 
-
-def is_muted():
-	process = subprocess.Popen(['amixer -c 1 sget Mic'], shell=True, stdout=subprocess.PIPE )
-	output = process.communicate()[0].split("\n")
-	is_mute = False
-
-	for line in output:
-	    data = line.lower()    
-	    if ('mono: capture' in data) and '[off]' in data:
-	        is_mute = True  
-	return is_mute
 
 def mute():
 	subprocess.Popen(['amixer -c 1 sset Mic nocap'], shell=True, stdout=subprocess.PIPE)
@@ -59,49 +22,34 @@ def unmute():
 	subprocess.Popen(['amixer -c 1 sset Mic cap'], shell=True, stdout=subprocess.PIPE )
 
 if __name__ == '__main__':
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-	strip.begin()
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(INPUT_PIN, GPIO.IN)
+	GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+	GPIO.output(OUTPUT_PIN, False)	
 
 	while True:
-		if FRAME*100 % 1 == 0:
-			wave(strip, FRAME)
-		FRAME+= 0.0010
-		if HORNTIMER > 8000:
-			HORN_LIFTED = not GPIO.input(INPUT_PIN)
-			muted = is_muted()
-			if HORN_LIFTED and muted:
-				unmute()
-			elif not HORN_LIFTED and not muted:
-				mute()
-			HORNTIMER = 0
+		HORN_LIFTED = not GPIO.input(INPUT_PIN)
+		if HORN_LIFTED:
+			unmute()
 		else:
-			HORNTIMER += 1
+			mute()
 		if HORN_LIFTED:
 			if RINGTIMER == 0:
-				requests.get("http://192.168.0.2:4567/?ring")
+				try:
+					r = requests.get("http://192.168.0.2:4567/?ring")
+				except requests.exceptions.RequestException as e:
+					m = 0 # do nothing
 			RINGTIMER += 1
-			if RINGTIMER > 8000:
+			if RINGTIMER > 6:
 				RINGTIMER = 0
-		if RING and not HORN_LIFTED:
-			AMP += 0.00005		
-			TIMER += 1
-		else:
-			AMP -= 0.0001
-		if AMP > 0.5:
-			AMP = 0.5
-		elif AMP < 0:
-			AMP = 0
-
-		if FRAME % 1 == 0:
-			FRAME = 0
-
-		if TIMER > 5000:
+		if TIMER > 6:
 			TIMER = 0
 			RING = False
+		else:
+			TIMER += 1
 
 		if os.path.exists('ring'):
 			RING = True
 			TIMER = 0
 			os.remove('ring')
+		time.sleep(0.5)
--- switchtest.py
+++ switchtest.py
@@ -6,15 +6,14 @@ import RPi.GPIO as GPIO
 from math import *
 from neopixel import *
 
-INPUT_PIN = 27
+INPUT_PIN = 23
 
 if __name__ == '__main__':
 	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(INPUT_PIN, GPIO.IN)
+	GPIO.setup(INPUT_PIN, GPIO.OUT)
 	timeout = 0
 	while timeout < 10000:
-		if GPIO.input(INPUT_PIN):
-			print "ON"
-		else:
-			print "OFF"
+		GPIO.output(INPUT_PIN, 1)
 		timeout+=1
+	
+	GPIO.output(INPUT_PIN, 0)
